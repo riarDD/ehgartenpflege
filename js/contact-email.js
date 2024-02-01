9e405407447a9b833f1eda8fc413a175b552d7dd
riarD <149739620+riarDD@users.noreply.github.com>
2024-02-01 20:13:13 +0000

    Add files via upload

---
+++ js/contact-email.js
@@ -0,0 +1,22 @@
+function sendMail() {
+  let params = {
+    name: document.getElementById("name").value,
+    email: document.getElementById("email").value,
+    message: document.getElementById("message").value,
+  };
+
+  const serviceID = "service_9iv0wje";
+  const templateID = "template_tmem11h";
+
+    emailjs.send(serviceID, templateID, params)
+    .then(res=>{
+        document.getElementById("name").value = "";
+        document.getElementById("email").value = "";
+        document.getElementById("message").value = "";
+        console.log(res);
+        alert("Your message sent successfully!!")
+
+    })
+    .catch(err=>console.log(err));
+
+}
